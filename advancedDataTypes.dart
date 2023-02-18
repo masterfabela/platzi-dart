c1e02275c44fde2e668fe41a452d258abd4cb731
Jose Ramon Vieites Romay <jose.vieites@altia.es>
2023-02-18 01:31:41 +0100

    Clase 6

---
+++ advancedDataTypes.dart
@@ -0,0 +1,35 @@
+void main() {
+  varExample();
+  dynamicExample();
+}
+
+void varExample() {
+  // Can't change the type of the variable
+  var name = "Ismael";
+  // name = 12; // Error
+  print(name);
+  name = "Jorge";
+  print(name);
+}
+
+void dynamicExample() {
+  // Can change the type of the variable
+  dynamic name = "Ismael";
+  print(name);
+  name = 12;
+  print(name);
+}
+
+void constExample() {
+  // Can't change the value of the variable
+  const name = "Ismael";
+  print(name);
+  // name = "Jorge"; // Error
+}
+
+void finalExample() {
+  // Can't change the value of the variable
+  final name = "Ismael";
+  print(name);
+  // name = "Jorge"; // Error
+}
